25c19245808f812fcbee46a5a12a6c339bb70134
Alexander Jenkins <acjenkins6440@gmail.com>
2017-07-27 16:35:26 -0700

    added a couple functions for the timer and polished it a little

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 var minutes = 0;
 var seconds = 0;
 var brk = 5;
-$("#break+").click(function(){
-  if(((parseInt($("#break").text()))+1) > 9){
-    $("#break").text(' ' +((parseInt($("#break").text()))+1))
+var timemin = 25;
+var timesec = '00';
+var sound = new Audio("demonstrative.mp3");
+var timerOn = false;
+var time = timemin + ':' + timesec;
+$(document).ready(function(){
+  bindClickHandlers();
+  $(".timer").text(time);
+});
+function bindClickHandlers(){
+  $("#breakinc").click(brkinc);
+  $("#breakdec").click(brkdec);
+  $("#timeinc").click(timeinc);
+  $("#timedec").click(timedec);
+  $(".timer").click(startTimer);
+}
+function updateTime(){
+  time = timemin + ':' + timesec;
+  $(".timer").text(time);
+}
+function brkinc(){
+  if(parseInt($("#break").text()) > 49){
   }
-  else{
-  $("#break").text(' ' +((parseInt($("#break").text()))+1) + ' ');
-}});
+  else{$("#break").text(' ' +((parseInt($("#break").text()))+1) + ' ');
+}};
 
-$("#break-").click(function(){
+function brkdec(){
   if(parseInt($("#break").text()) == 1){
   }
-  else if(parseInt($("#break").text()) > 10){
-    $("#break").text(' ' +((parseInt($("#break").text()))-1));
-  }
   else{$("#break").text(' ' +((parseInt($("#break").text()))-1) + ' ');
-}});
-/* Do up and down arrows instead of + and -
-https://www.w3schools.com/howto/howto_js_countdown.asp
-https://www.w3schools.com/bootstrap/bootstrap_progressbars.asp*/
+}};
+function timeinc(){
+  if(parseInt($("#timer").text()) > 59){}
+  else{
+    $("#timer").text(" " + ((parseInt($("#timer").text())) +1));
+  }
+  timemin=parseInt($("#timer").text());
+  updateTime();
+};
+function timedec(){
+  if(parseInt($("#timer").text()) == 1){
+  }
+  else{$("#timer").text(' ' +((parseInt($("#timer").text()))-1));}
+  timemin = parseInt($("#timer").text());
+  updateTime();
+};
+function workTimer(){
+  if(timesec == '00' && timemin !== 0){
+    timesec = '59';
+    timemin -= 1;
+  }
+  else if(timemin === 0 && timesec=="00"){
+    $(".session").text('Break!');
+  }
+}
+function startTimer(){
+  if(timerOn === false){
+    $("#breakinc").off('click');
+    $("#breakdec").off('click');
+    $("#timeinc").off('click');
+    $("#timedec").off('click');
+    timerOn = true;
+
+  }
+  else{
+    bindClickHandlers();
+    timerOn = false;
+  }
+}
